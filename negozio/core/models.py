aabd0dc25916215d4799f9bd39bd7fdbb11c85c8
Andrea Monari <amonari1@tutamail.com>
2026-03-05 17:01:07 +0100

    installazione axios + creazione form ordini

--- negozio/core/models.py
+++ negozio/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return f"{self.email}"
