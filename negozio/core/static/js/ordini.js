aa12c5b898903430aad5a404b2b7571abf52b562
Andrea Monari <amonari1@tutamail.com>
2026-03-09 02:50:27 +0100

    integrazione validazione input form

--- negozio/core/static/js/ordini.js
+++ negozio/core/static/js/ordini.js
@@ -1,77 +1,132 @@
-
+let dettagliProdotti = [];
 
 document.addEventListener("DOMContentLoaded", async () => {
-
     const token = document.querySelector('[name=csrfmiddlewaretoken]').value;
     axios.defaults.headers.common['X-CSRFToken'] = token;
     
     await caricaClienti();
     await caricaProdotti();
 
-    
-    
+    document.getElementById("btnAggiungiProdotto").addEventListener("click", () => {
+        const prodotto = document.getElementById('selectProdotto');
+        const quantita = document.getElementById('quantitaProdotto').value;
 
-});
+        const prodottoId = prodotto.value;
+        const prodottoSelezionato = prodotto.options[prodotto.selectedIndex].text;
 
-let dettagliProdotti = [];
-document.getElementById("btnAggiungiProdotto").addEventListener("click", () => {
+        if(!prodottoId) {
+            alert('Errore! non hai selezionato nessun prodotto.');
+            return;
+        }
+        if(!quantita) {
+            alert('Errore! la quantità non può essere minore di 1.')
+            return;
+        }
 
-    const prodotto = document.getElementById('selectProdotto');
-    const quantita = document.getElementById('quantitaProdotto').value;
+        for(const d of dettagliProdotti) {
+            if(d.prodotto == prodottoId) {
+                alert("Errore! prodotto già inserito. Eliminalo prima se vuoi modificarne la quantità.");
+                return;
+            }
+        }
 
-    const prodottoId = prodotto.value;
-    const prodottoSelezionato = prodotto.options[prodotto.selectedIndex].text;
+        const prodottoString = prodottoSelezionato.split(" - €");
+        const prodottoNome = prodottoString[0];
+        const prodottoPrezzo = prodottoString[1];
 
-    const prodottoString = prodottoSelezionato.split(" - €");
-    const prodottoNome = prodottoString[0];
-    const prodottoPrezzo = prodottoString[1];
+        // controllo se il prodotto selezionato è già stato inserito
+        //...
 
+        const dettaglio = {
+            prodotto : parseInt(prodottoId),
+            quantita : parseInt(quantita),
+            prezzo_unitario : prodottoPrezzo
+        };
 
-    if(!prodottoId) {
-        alert('Errore! non hai selezionato nessun prodotto.');
-        return;
-    }
-    if(!quantita) {
-        alert('Errore! la quantità non può essere minore di 1.')
-        return;
-    }
+        dettagliProdotti.push(dettaglio);
+        
 
-    // controllo se il prodotto selezionato è già stato inserito
-    //...
+        const table = document.getElementById('tableDettagli');
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${prodottoNome}</td>
+            <td>${quantita}</td>
+            <td>${prodottoPrezzo}</td>
+            <td>
+                <button class="btn btn-danger btnDeleteProdotto">X</button>
+            </td>
+        `;   
+        
+        tr.querySelector('.btnDeleteProdotto').addEventListener("click", () => {
+            tr.remove();
+            // prendo l'indice del tr appena aggiunto
+            const index = dettagliProdotti.indexOf(dettaglio);
+            // e lo passo a splice così da rimuoverlo (va all'indice index ed elimina 1 elemento)
+            dettagliProdotti.splice(index, 1);
+            aggiornaDettagliProdotti();
+        })
 
-    const dettaglio = {
-        prodotto : parseInt(prodottoId),
-        quantita : parseInt(quantita),
-        prezzo_unitario : prodottoPrezzo
-    }
+        table.append(tr); 
 
-    dettagliProdotti.push(dettaglio);
-    
-
-    const table = document.getElementById('tableDettagli');
-    const tr = document.createElement('tr');
-    tr.innerHTML = `
-        <td>${prodottoNome}</td>
-        <td>${quantita}</td>
-        <td>${prodottoPrezzo}</td>
-        <td>
-            <button class="btn btn-danger btnDeleteProdotto">X</button>
-        </td>
-    `;   
-    
-    tr.querySelector('.btnDeleteProdotto').addEventListener("click", () => {
-        tr.remove();
-        // prendo l'indice del tr appena aggiunto
-        const index = dettagliProdotti.indexOf(dettaglio)
-        // e lo passo a splice così da rimuoverlo (va all'indice index ed elimina 1 elemento)
-        dettagliProdotti.splice(index, 1);
         aggiornaDettagliProdotti();
-    })
 
-    table.append(tr); 
+    });
+    
+    document.getElementById("formOrdine").addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-    aggiornaDettagliProdotti();
+        if(dettagliProdotti.length == 0) {
+            alert("Errore! aggiungi almeno un prodotto")
+            return;
+        }
 
+        const cliente = document.getElementById('selectCliente').value;
+
+        const ordine = {
+            cliente : cliente,
+        };
+        
+        axios.post('/api/ordini/', {
+            ordine,
+            dettagli : dettagliProdotti
+        })
+            .then(response => {
+                alert("Successo! ordine creato correttamente.");
+                window.location.reload();
+            })
+            .catch(error => {
+                alert("Errore! durante la creazione dell'ordine");
+                console.log(error);
+            })
+    });
+
+
+    document.querySelectorAll('.btnDeleteOrdine').forEach(btn => {
+        btn.addEventListener("click", () => {
+            if(!confirm("Confermi l'eliminazione?")) {
+                alert("Processo annullato");
+                return;
+            }
+
+            const id_ordine = btn.dataset.idOrdine;
+            axios.delete(`/api/ordini/${id_ordine}/`)
+                .then(response => {
+                    alert("Eliminazione riuscita");
+                    window.location.reload();
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            
+
+        });
+    });
+
+    document.getElementById("btnAnnulla").addEventListener("click", () => {
+        resetForm();
+        dettagliProdotti = []
+        aggiornaDettagliProdotti()
+    });
 });
 
 async function caricaClienti() {
@@ -101,72 +156,17 @@ async function caricaProdotti() {
         option.value = p.id;
         option.textContent = `${p.nome} - €${p.prezzo}`;
         select.append(option);
-    })
+    });
 }
-
-document.getElementById("formOrdine").addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    if(dettagliProdotti.length == 0) {
-        alert("Errore! aggiungi almeno un prodotto")
-        return;
-    }
-
-    const cliente = document.getElementById('selectCliente').value;
-
-    const ordine = {
-        cliente : cliente,
-    };
-    
-    axios.post('/api/ordini/', {
-        ordine,
-        dettagli : dettagliProdotti
-    })
-        .then(response => {
-            alert("Ordine creato correttamente!")
-            window.location.reload();
-        })
-        .catch(error => {
-            alert("Errore durante la creazione dell'ordine")
-            console.log(error)
-        })
-});
-
-
-document.querySelectorAll('.btnDeleteOrdine').forEach(btn => {
-    btn.addEventListener("click", () => {
-        if(!confirm("Confermi l'eliminazione?")) {
-            alert("Processo annullato");
-            return;
-        }
-
-        const id_ordine = btn.dataset.idOrdine;
-        axios.delete(`/api/ordini/${id_ordine}/`)
-            .then(response => {
-                alert("Eliminazione riuscita");
-                window.location.reload();
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        
-
-    })
-})
-
-document.getElementById("btnAnnulla").addEventListener("click", () => {
-    resetForm();
-    dettagliProdotti = []
-    aggiornaDettagliProdotti()
-})
 
 function resetForm() {
     const form = document.getElementById('formOrdine');
-    form.reset()
+    form.reset();
 }
 
 function aggiornaDettagliProdotti() {
     const divDettagli = document.getElementById('divDettagli');
+
     if(dettagliProdotti.length == 0) {
         const table = document.getElementById('tableDettagli');
         table.innerHTML = '';
